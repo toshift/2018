6c36229837de64400b37903f60a54a8f60379b54
tosh1ft <jig.yamauchi@gmail.com>
2018-01-17 13:18:56 +0900

    ブロックチェーンとプルーフオブワークアルゴリズムについて1

---
+++ 0117/blockchain.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+import hashlib
+import json
+from time import time
+
+"""
+・チェーンはトランザクションを収納
+・新しいブロックはチェーンに加える
+・ハッシュ : メッセージを特定するための暗号化技術
+・トランザクション : データベース管理システム（または類似のシステム）内で実行される、分けることのできない一連の情報処理の単位
+・プルーフオブワーク ： 
+"""
+
+class BlockChain(object):
+    def __init__(self):
+        # チェーン
+        self.chain = []
+        # トランザクション
+        self.current_transactions = []
+        
+        # ジェネシスブロックの生成
+        self.new_block(previous_hash=1, proof=100)
+    
+    def new_block(self, previous_hash=None, proof):
+        """
+        新しいブロックの生成、チェーンに加える
+        @param previous_hash: <str> 前のブロックのハッシュ
+        @param proof: <int> プルーフオブワークアルゴリズムから得られるプルーフ
+        @return: <dict> 新しいブロック
+        """
+
+        block = {
+            'index': len(self.chain) + 1
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # 現在のトランザクションリストをリセット
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        新しいトランザクションをリストに加える
+        @ param sender: <str> 送信者のアドレス
+        @ param recipient: <str> 受信者のアドレス
+        @ param amount: <int> 量
+        @ return <int> このトランザクションを含むブロックのアドレス
+        """
+        self.current_transactions({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        ## インデックスは必ず1追加でいいのか?
+        return self.last_block['index'] += 1
+    
+    @staticmethod
+    def hash(block):
+        """
+        ブロックをハッシュ化する
+        @ param block: <dict> ブロック
+        @ return <str>
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha224(block_string).hexdigest()
+    
+    @property
+    def last_block(self):
+        """
+        チェーンの最後のブロックをリターンする
+        """
+        return self.chain[-1]
+
---
+++ 0117/proof_of_work.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+import hashlib
+import json
+
+class Proof_of_work:
+    """
+    プルーフオブワークアルゴリズムのためのクラス
+    """
+    def algo(self, last_proof):
+        """
+        プルーフオブワークのアルゴリズム
+        ハッシュ値検索の条件を加えて、そのハッシュ値が条件通りかどうかをチェックする
+        """
+        proof = 0
+        while self.validation_proof(last_proof, proof) is False:
+            proof += 1
+        
+
+    @staticmethod
+    def validation_proof(last_proof, proof):
+        """
+        proofが正しいか確認する
+        アルゴリズムの難易度はここで決まる
+        @ param last_proof: <int> 前のproof
+        @ param proof: <int> 現在のproof
+        @ return <bool> 
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        print(guess)
+        guess_hash = hashlib.sha224(guess).hexdigest()
+        print(guess_hash)
+
+        return guess_hash[-1] == '0'
+
+if __name__ == '__main__':
+    pOw = Proof_of_work()
+    pOw.algo(0)
