3daf5b63e6861c00db908a11cb6c6727b1aa1c73
tosh1ft <jig.yamauchi@gmail.com>
2018-02-01 21:47:06 +0900

    ps dir作成自動化

---
+++ 0201/sample.ps1
@@ -0,0 +1,24 @@
+﻿# Powershell有効コマンド
+# Set-ExecutionPolicy RemoteSigned 
+
+# 今日のプログラミング用のディレクトリを作成する
+
+### 共通データ定義
+#[String[]]$PATH = C:\work\github\2018
+# Set-Locationにパラメータを渡したかったのに渡せなかったので、方法を探し中。
+
+function MakeDir($name) {
+    if ((Test-Path $name) -eq $False ){
+        New-Item -ItemType Directory -Path $name
+        return
+    }
+}
+
+function Main {
+    # gitのpathに変更する
+    Set-Location C:\work\github\2018
+    $dir_name = (Get-Date).ToString("MMdd")
+    MakeDir($dir_name)
+}
+
+Main;
