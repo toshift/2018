6c2930b96e145c94bd6cfbd8846c60fd35d998a6
yamauchi toshikazu <yamauchi@JIG-SAW.COM>
2018-02-02 20:15:10 +0900

    powershell 引数の指定の違い

---
+++ 0202/func.ps1
@@ -0,0 +1,14 @@
+function ParamCheck($a, $b){
+    $a
+    $b
+}
+
+function ParamArray($c){
+    $c
+}
+function Main {
+    ParamCheck "testString" 2
+    ParamArray("paramstring", 1, 3)
+}
+
+Main
